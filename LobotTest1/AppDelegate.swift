abe7891330051cb3be3228d08b106f02c6775a10
Toru Ishihara <torui@nariuchi.com>
2019-03-02 10:20:26 -0800

    Basic movement implemented

---
+++ LobotTest1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LobotTest1
+//
+//  Created by sbdev on 2/2/19.
+//  Copyright © 2019 Actiontec. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
